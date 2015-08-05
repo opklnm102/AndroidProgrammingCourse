3621716c24cc62b2deec60d15b6eb60751efc637
Minyoung <skplanet@192.168.205.119>
2015-08-05 17:48:03 +0900

    Day 3 Final

---
+++ AndroidComponent/src/com/example/androidcomponent/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.androidcomponent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+	EditText inputView;
+	TextView resultView;
+	private static final int CODE_OTHER = 0;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		inputView = (EditText)findViewById(R.id.editText1);
+		resultView = (TextView)findViewById(R.id.text_result);
+		Button btn = (Button)findViewById(R.id.button1);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+				String input = inputView.getText().toString();
+				intent.putExtra(OtherActivity.EXTRA_MESSAGE, input);
+				startActivityForResult(intent, CODE_OTHER);
+				
+			}
+		});
+	}
+
+	@Override 
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == CODE_OTHER && resultCode == Activity.RESULT_OK) {
+			String message = data.getStringExtra(OtherActivity.RESULT_MESSAGE);
+			resultView.setText(message); 
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
